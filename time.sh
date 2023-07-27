f1c31e90d1a330bc93fe38cd9ef990d43c30a5d9
Zest-Zhang <2521946696zzt@gmail.com>
2023-07-27 19:40:05 +0800

    2023-07-27

---
+++ time.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+#获取当前日期
+TODAY=$(date +%Y-%m-%d)
+
+git add .
+# 配置默认字符集为 UTF-8，避免 commit 乱码
+git config --global i18n.commitencoding utf-8
+git commit -m "$TODAY"
+git push origin gh-pages
