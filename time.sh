370885dd490f740757744234c5568ef65725e8c2
Zest-Zhang <2521946696zzt@gmail.com>
2023-08-27 14:53:39 +0800

    8.27

--- time.sh
+++ time.sh
@@ -5,7 +5,5 @@ TODAY=$(date +%Y-%m-%d)
 
 git checkout gh-pages
 git add .
-# 配置默认字符集为 UTF-8，避免 commit 乱码
-git config --global i18n.commitencoding utf-8
 git commit -m "$TODAY"
 git push origin gh-pages -f
